2d8db01239ff8c63dd9574d57d8f4afe78fe354a
James Crockett <jecrockett@gmail.com>
2018-09-06 23:09:20 -0400

    test

--- milfbot/commands/team.rb
+++ milfbot/commands/team.rb
@@ -8,6 +8,7 @@ module Milfbot
 
         owner = helper.matched_owner_name(match[:expression])
         team_id = Constants::ESPN_TEAM_IDS_BY_OWNER[owner]
+        
         url = "http://games.espn.com/ffl/clubhouse?leagueId=#{Constants::ESPN_LEAGUE_ID}&teamId=#{team_id}&seasonId=#{Constants::ESPN_LEAGUE_YEAR}"
         page = Nokogiri::HTML(HTTParty.get(url))
         players = page.css('.playertablePlayerName').map { |row| row.text }.join("\n")
