d821c256232882f93dffc8aaca5e61bfdca963f8
Samuel G Grant <samuelggrant@Samuels-MacBook-Pro.local>
2018-09-30 15:05:52 -0400

    Fixed all code suggestions, and odds now gets sorted

--- milfbot/commands/odds.rb
+++ milfbot/commands/odds.rb
@@ -6,17 +6,19 @@ module Milfbot
       command 'odds' do |client, data, match|
         url = "https://script.google.com/macros/s/AKfycbxxuThpfWY1dZ6Typd9AyeyYlPmCtVUOsCY5qnCPU3npIK-26sT/exec?command=odds"
         response = HTTParty.get(url, format: :plain)
-        JSON.parse response, symbolize_names: true
+        json = JSON.parse(response, symbolize_names: true)
 
-        owners = eval(response)[:owner]
-        odds = eval(response)[:odds]
+		odds = []
+		json[:owner].flatten().each_with_index do |owner, index|
+			odds << {name: owner, odds: json[:odds].flatten()[index]}
+		end
+    	odds = odds.sort_by {|h| -h[:odds]}
+
         message = []
         message << '*Playoff Odds:*'
         message << ''
-        i = 0
-        owners.each do |owner|
-          message << "*#{owner[0]}*: #{(odds[i][0]*100).round(2)}%"
-          i += 1
+        odds.each do |entry|
+          message << "*#{entry[:name]}*: #{(entry[:odds]*100).round(2)}%"
         end
 
         client.say(text: message.flatten.compact.join("\n"), channel: data.channel)
--- milfbot/commands/rankings.rb
+++ milfbot/commands/rankings.rb
@@ -6,17 +6,15 @@ module Milfbot
       command 'rankings' do |client, data, match|
         url = "https://script.google.com/macros/s/AKfycbxxuThpfWY1dZ6Typd9AyeyYlPmCtVUOsCY5qnCPU3npIK-26sT/exec?command=ranks"
         response = HTTParty.get(url, format: :plain)
-        JSON.parse response, symbolize_names: true
+        json = JSON.parse response, symbolize_names: true
 
-        owners = eval(response)[:owner]
-        ranks = eval(response)[:ranks]
+        owners = json[:owner].flatten()
+        ranks = json[:ranks].flatten()
         message = []
         message << '*Power Rankings:*'
         message << ''
-        i = 0
-        owners.each do |owner|
-          message << "*#{owner[0]}*: ##{ranks[i][0]}"
-          i += 1
+        owners.each_with_index do |owner,index|
+          message << "*#{owner}*: ##{ranks[index]}"
         end
 
         client.say(text: message.flatten.compact.join("\n"), channel: data.channel)
