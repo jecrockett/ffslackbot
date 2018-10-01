bd30e31739d637526692deb89232ddca95540fbe
sggrant89 <sggrant89@gmail.com>
2018-10-01 14:37:31 -0400

    Update milf_sim.rb
    realized i couldn't dynamically adjust scheduler with daylight savings so now simulation will run at 9am thursdays during dst and 8am thursdays otherwise

--- milfbot/services/milf_sim.rb
+++ milfbot/services/milf_sim.rb
@@ -6,12 +6,8 @@ class MilfSim
   end
 end
 
-offset = TZInfo::Timezone.get("America/New_York").current_period.offset.utc_total_offset/3600
-hour = 12-offset
-
 simulation = Rufus::Scheduler.new
-
-simulation.cron "36 #{hour} * * fri" do
+simulation.cron "0 13 * * thu" do
   print 'Simulating the rest of the season...'
   MilfSim.simulate_MILF
   puts 'done!'
