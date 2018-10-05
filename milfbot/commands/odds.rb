5fda19e814357dbdcfead5f2fa930e76f2221e88
Samuel G Grant <samuelggrant@samuels-imac.lan>
2018-10-05 12:59:32 -0400

    fixed odds.rb indents

--- milfbot/commands/odds.rb
+++ milfbot/commands/odds.rb
@@ -8,20 +8,20 @@ module Milfbot
         response = HTTParty.get(url, format: :plain)
         json = JSON.parse(response, symbolize_names: true)
 
-	odds = []
-	json[:owner].flatten().each_with_index do |owner, index|
-		odds << {name: owner, odds: json[:odds].flatten()[index]}
-	end
+    	odds = []
+    	json[:owner].flatten().each_with_index do |owner, index|
+    		odds << {name: owner, odds: json[:odds].flatten()[index]}
+    	end
     	odds = odds.sort_by {|h| -h[:odds]}
 
-        message = []
-        message << '*Playoff Odds:*'
-        message << ''
-        odds.each do |entry|
-          message << "*#{entry[:name]}*: #{(entry[:odds]*100).round(2)}%"
-        end
+      message = []
+      message << '*Playoff Odds:*'
+      message << ''
+      odds.each do |entry|
+        message << "*#{entry[:name]}*: #{(entry[:odds]*100).round(2)}%"
+      end
 
-        client.say(text: message.flatten.compact.join("\n"), channel: data.channel)
+      client.say(text: message.flatten.compact.join("\n"), channel: data.channel)
       end
     end
   end
